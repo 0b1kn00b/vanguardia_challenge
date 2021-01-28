8bd71c19fc375842e25cdef225ae418d69056db7
dayz <polysemantic@gmail.com>
2021-01-28 07:17:31 +0100

    websockets

--- src/main/haxe/auction/client/ws/Request.hx
+++ src/main/haxe/auction/client/ws/Request.hx
@@ -7,7 +7,15 @@ package auction.client.ws;
   public final user : UserRequestSchema;
 }
 class UserRequestSchema extends Clazz{
+  var mock : Array<SignInForm>;
+  
+  public function new(){
+    this.mock : Array<SignInForm> = __.resource('users').data;
+  }
   public function sign_in(form:SignInForm){
     return SignIn(form);
+  }
+  public var user_one(){
+    return sign_in(this.mock[0]);
   }
 }
